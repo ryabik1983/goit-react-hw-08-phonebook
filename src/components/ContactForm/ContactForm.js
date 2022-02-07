a738a398a3697f23781be2e2347b64b5a720e8cf
ryabik1983 <81900360+ryabik1983@users.noreply.github.com>
2022-02-07 06:10:48 +0200

    hw8
    hw8

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -55,7 +55,7 @@ const ContactForm = ({ onSubmit }) => {
           className={styles.input}
           type="text"
           name="name"
-          placeholder="Liubov Murarova"
+          placeholder="Ivan Dulin"
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           inputProps={ariaLabel}
@@ -69,7 +69,7 @@ const ContactForm = ({ onSubmit }) => {
           name="number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          placeholder="099-999-99-99"
+          placeholder="067-233-15-14"
           required
           value={number}
           onChange={handleChange}
